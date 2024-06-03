543f946f2934d06bd33fa0efa979b4547db05483
KaiserYolo <Lucassanner04@gmail.com>
2024-06-03 19:47:30 +0200

    creaTION

---
+++ core/src/com/mygdx/game/Main.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	//Skin skin = new Skin();
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		//TextButton button = new TextButton("Click me!", skin, "bigButton");
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
